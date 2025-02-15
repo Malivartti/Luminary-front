3a45f67616f8b7c21a3e62a7fdb27b2d78f4a66a
malivartti <malivartti@yandex.ru>
2025-02-15 21:38:57 +0300

    rebranding

--- src/widgets/Header/ui/Logo/Logo.tsx
+++ src/widgets/Header/ui/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
   return (
     <Link to={AppRoutes.MAIN} >
-      <Text view='p-20' weight='bold'>AI System</Text>
+      <Text view='p-20' weight='bold'>Luminary</Text>
     </Link>
   );
 };
