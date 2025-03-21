34a0c5806f3ce426aa5384811349f5789d45d985
malivartti <malivartti@yandex.ru>
2025-03-21 13:16:33 +0300

    fix deploy

--- src/shared/config/api/index.ts
+++ src/shared/config/api/index.ts
@@ -1,2 +1,3 @@
 // export const API_ENTRY_POINT = process.env.API_ROOT || 'http://127.0.0.1:8000';
-export const API_ENTRY_POINT = 'http://46.17.102.244';
+// export const API_ENTRY_POINT = 'http://46.17.102.244';
+export const API_ENTRY_POINT = 'https://claustro.tech';
