26a5be72392e50909247bce28e2b7211d7e9c415
malivartti <malivartti@yandex.ru>
2025-03-21 06:44:22 +0300

    api

--- src/shared/config/api/index.ts
+++ src/shared/config/api/index.ts
@@ -1,1 +1,2 @@
-export const API_ENTRY_POINT = process.env.API_ROOT || 'http://127.0.0.1:8000';
+// export const API_ENTRY_POINT = process.env.API_ROOT || 'http://127.0.0.1:8000';
+export const API_ENTRY_POINT = 'http://46.17.102.244';
